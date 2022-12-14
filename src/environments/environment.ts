ef501668a5a75d8a939aa6ca6763493ba02ede70
MarielaM20 <m.mircheva02@gmail.com>
2022-12-14 23:55:23 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,18 +2,25 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyCJQocr2odU0A0Q-FZr5GRbDrPqdPIRvU8",
-    authDomain: "beautysalon-ec694.firebaseapp.com",
-    databaseURL: "https://beautysalon-ec694-default-rtdb.firebaseio.com",
-    projectId: "beautysalon-ec694",
-    storageBucket: "beautysalon-ec694.appspot.com",
-    messagingSenderId: "569069136689",
-    appId: "1:569069136689:web:2cef5f28b18a7edd181e10"
-  }
+  apiURL: 'https://jsonplaceholder.typicode.com'
 };
+
+
+// export const environment = {
+//   production: false,
+//   firebase: {
+//     apiKey: "AIzaSyCJQocr2odU0A0Q-FZr5GRbDrPqdPIRvU8",
+//     authDomain: "beautysalon-ec694.firebaseapp.com",
+//     databaseURL: "https://beautysalon-ec694-default-rtdb.firebaseio.com",
+//     projectId: "beautysalon-ec694",
+//     storageBucket: "beautysalon-ec694.appspot.com",
+//     messagingSenderId: "569069136689",
+//     appId: "1:569069136689:web:2cef5f28b18a7edd181e10"
+//   }
+// };
 
 /*
  * For easier debugging in development mode, you can import the following file
