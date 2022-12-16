5c0b2d30cbaa9de101e6b2d6444fa5848b7e9758
MarielaM20 <m.mircheva02@gmail.com>
2022-12-16 22:08:23 +0200

    Update auth.service.ts

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IUser } from '../shared/interfaces';
 
@@ -6,11 +7,26 @@ import { IUser } from '../shared/interfaces';
 })
 export class AuthService {
 
-  user: IUser | null = null;
+  //user: IUser | null = null;
+
+  user: IUser | null = {
+    firstName: "m",
+    lastName: "m",
+    email: "m",
+    phone: "m",
+  };
 
   get isLoggedIn() {
     return this.user !== null;
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(firstName: string, lastName: string, email: string, phone: string, password: string, rePassword: string) {
+    return this.http.post<any>('/api/register', { firstName, lastName, email, phone, password, rePassword });
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<any>('/api/login', {email, password });
+  }
 }
